5d1d4bf3463733e47855e3e3dc185e3710ad92aa
Tetsuya Shioda <shiodat.cc@gmail.com>
2020-08-09 23:59:41 +0900

    update local api client

--- feelbot/api.py
+++ feelbot/api.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from .client import Client
 from .models import Lesson
+from loguru import logger
 
 
 app = FastAPI()
@@ -13,10 +14,13 @@ app = FastAPI()
 async def find_lesson(
     studio: str,
     schedule: datetime,
-    polling: bool = False
+    polling: bool = False,
+    sleep: int = 30
 ):
     with Client() as client:
-        lesson = client.find_lesson(studio, schedule, polling=polling)
+        lesson = client.find_lesson(studio, schedule, polling=polling, sleep=int(sleep))
+    pref='lesson information\n'
+    incoming_webhook(lesson.text(prefix=pref))
     return lesson
 
 
@@ -24,11 +28,15 @@ async def find_lesson(
 async def reserve_lesson(
     studio: str,
     schedule: datetime,
-    polling: bool = False
+    polling: bool = False,
+    sleep: int = 30
 ):
     with Client() as client:
         success, lesson = client.reserve_lesson(
-            studio, schedule, relocate=False, polling=polling)
+            studio, schedule, relocate=False, polling=polling, sleep=int(sleep))
+
+    pref = 'reservation success!\n' if success else 'reservation failed\n'
+    incoming_webhook(lesson.text(prefix=pref))
     return lesson
 
 
@@ -36,9 +44,20 @@ async def reserve_lesson(
 async def reserve_lesson(
     studio: str,
     schedule: datetime,
-    polling: bool = False
+    polling: bool = False,
+    sleep: int = 30
 ):
     with Client() as client:
         success, lesson = client.reserve_lesson(
-            studio, schedule, relocate=True, polling=polling)
+            studio, schedule, relocate=True, polling=polling, sleep=int(sleep))
+
+    pref = 'reservation success!\n' if success else 'reservation failed\n'
+    incoming_webhook(lesson.text(prefix=pref))
     return lesson
+
+
+def incoming_webhook(message):
+    logger.info('webhook response\n' + message)
+    requests.post(
+        os.environ.get('FEELCYCLE_BOT_INCOMING_WEBHOOK'),
+        data=json.dumps({'text': message}).encode('utf-8'))
